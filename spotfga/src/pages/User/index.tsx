4577faae52d140e74dbf5aedff8c1f2d9b165658
gabriel-paiva <gabrielpaivaaguiar.gp@gmail.com>
2021-05-09 11:33:14 -0300

    feat: add fields for sugestions

--- spotfga/src/pages/User/index.tsx
+++ spotfga/src/pages/User/index.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react';
+import React, { useContext, useCallback } from 'react';
 
+import { IoMdPersonAdd, IoIosPlayCircle } from 'react-icons/io';
 import { useParams, useHistory } from 'react-router-dom';
 
-import { Container, Content, Button } from './styles';
+import { Container, Content, Button, Sugestion, ButtonHeader } from './styles';
 
 import Card from '../../components/Card';
 import Header from '../../components/Header';
@@ -21,6 +22,13 @@ const User: React.FC = () => {
 
   const user = graph.nodes.filter((iuser) => iuser.id === Number(id))[0];
 
+  const handleAddFriend = useCallback(
+    (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+      e.stopPropagation();
+    },
+    [],
+  );
+
   return (
     <Container>
       <Header />
@@ -37,12 +45,50 @@ const User: React.FC = () => {
             <Subtitle>{friend.nome}</Subtitle>
           </Button>
         ))}
+        <Sugestion>
+          <Subtitle style={{ marginBottom: 10 }}>
+            Amigos sugeridos:{' '}
+            <span style={{ fontSize: 10 }}>
+              (com base nas músicas que você ouviu)
+            </span>
+          </Subtitle>
+          <Button>
+            <Subtitle>Seu Jorge</Subtitle>
+            <ButtonHeader
+              style={{ marginRight: 10 }}
+              onClick={(e) => {
+                handleAddFriend(e);
+              }}
+            >
+              <IoMdPersonAdd size={30} />
+            </ButtonHeader>
+          </Button>
+        </Sugestion>
         <Title>Suas musicas:</Title>
         {user?.musics.map((music) => (
           <Button key={music} style={{ cursor: 'default' }}>
             <Subtitle>{music}</Subtitle>
           </Button>
         ))}
+        <Sugestion>
+          <Subtitle style={{ marginBottom: 10 }}>
+            Músicas sugeridas:{' '}
+            <span style={{ fontSize: 10 }}>
+              (com base nas suas músicas e de seus amigos)
+            </span>
+          </Subtitle>
+          <Button style={{ cursor: 'default' }}>
+            <Subtitle>Burguesinha</Subtitle>
+            <ButtonHeader
+              style={{ marginRight: 10 }}
+              onClick={(e) => {
+                handleAddFriend(e);
+              }}
+            >
+              <IoIosPlayCircle size={36} />
+            </ButtonHeader>
+          </Button>
+        </Sugestion>
       </Content>
     </Container>
   );
--- spotfga/src/pages/User/styles.ts
+++ spotfga/src/pages/User/styles.ts
@@ -26,6 +26,44 @@ export const Button = styled.button`
   background-color: transparent;
   cursor: pointer;
 
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 20px;
+
+  &:hover {
+    background-color: #ffffff24;
+  }
+`;
+
+export const Sugestion = styled.div`
+  width: 100%;
+  margin: 10px 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  background-color: #ffffff24;
+  border-radius: 8px;
+  padding: 20px;
+`;
+
+export const ButtonHeader = styled.button`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  svg {
+    fill: #fff;
+  }
+
   &:hover {
     background-color: #ffffff24;
   }
